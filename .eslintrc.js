9266545217d80692783fd5b3a358aa8955736dcf
Lars Reimann <mail@larsreimann.com>
2022-06-04 20:07:18 +0200

    fix: import rules not working

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,13 @@
 module.exports = {
     root: true,
     parserOptions: {
+        tsconfigRootDir: __dirname,
         project: './tsconfig.json',
+    },
+    settings: {
+        jest: {
+            version: 28,
+        },
     },
     extends: './index.js',
 };
